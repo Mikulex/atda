7d93ae506d638dde1415405144af85d481c7087a
Alexander Kühnle <alexander.kuehnle@uni-jena.de>
2021-09-03 13:18:53 +0200

    initial commit

---
+++ src/main/java/com/mikulex/atda/AtdaApplication.java
@@ -0,0 +1,13 @@
+package com.mikulex.atda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikulex/atda/AtdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikulex.atda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
