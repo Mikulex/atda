2e31311db5586309d8a34837ae5b5c3d7457cc76
Mikulex <mikulex@mikulex.com>
2021-09-07 20:38:28 +0200

    Write AccountModelAssembler

---
+++ src/main/java/com/mikulex/atda/AccountModelAssembler.java
@@ -0,0 +1,18 @@
+package com.mikulex.atda;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
+@Component
+public class AccountModelAssembler implements RepresentationModelAssembler<Account, EntityModel<Account>> {
+
+    @Override
+    public EntityModel<Account> toModel(Account account) {
+        return EntityModel.of(account, 
+        linkTo(methodOn(AccountController.class).one(account.getId())).withSelfRel());
+    }
+
+}
